4411c55d73dead03d602793973e38f83d985eb45
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2017-11-29 20:08:19 +0200

    Reaction game finished

---
+++ ReactionGame/ReactionGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ReactionGame
+{
+    partial class FormReaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(183, 320);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(536, 72);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ReactionGame.Properties.Resources._3;
+            this.pictureBox3.Location = new System.Drawing.Point(609, 21);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(248, 235);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ReactionGame.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(313, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(248, 235);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ReactionGame.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 235);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelHelp
+            // 
+            this.labelHelp.Location = new System.Drawing.Point(60, 265);
+            this.labelHelp.Name = "labelHelp";
+            this.labelHelp.Size = new System.Drawing.Size(760, 23);
+            this.labelHelp.TabIndex = 4;
+            this.labelHelp.Text = "Кликайте на картинки сразу после их появления";
+            this.labelHelp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 291);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(853, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // FormReaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 404);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelHelp);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormReaction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReactionGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelHelp;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ReactionGame/ReactionGame/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ReactionGame
+{
+    public partial class FormReaction : Form
+    {
+        int totalClicks = 10;
+        int clickNr = -1;
+        int reactTimeMs = 0;
+        int waiting = 0;
+        int minWaiting = 1;
+        int maxWaiting = 3;
+        int myCardNr=0;
+
+        Random rand = new Random();
+        Stopwatch watch = new Stopwatch();
+
+        public FormReaction()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = totalClicks;
+        }
+        public void ShowCard(int nr)
+        {
+            pictureBox1.Visible = nr == 1;
+            pictureBox2.Visible = nr == 2;
+            pictureBox3.Visible = nr == 3;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (clickNr < 0) return;
+            if (waiting > 0)
+            {
+                waiting--;
+                if (waiting == 0)
+                {
+                    myCardNr = rand.Next(1, 4);
+                    ShowCard(myCardNr);
+                    watch.Restart();
+                }
+            }         
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            clickNr = 0;
+            reactTimeMs = 0;
+            btnStart.Enabled = false;         
+            NextClick();
+        }
+        private void NextClick()
+        {
+            ShowCard(0);
+            clickNr++;
+            
+            waiting = rand.Next(minWaiting*1000/timer1.Interval, maxWaiting*1000/timer1.Interval + 1);
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            watch.Stop();
+            reactTimeMs += (int)watch.ElapsedMilliseconds;
+            progressBar1.Value = clickNr;
+            if (clickNr >= totalClicks)
+                ShowResult();
+            else
+                NextClick();
+           
+        }
+        private void ShowResult()
+        {
+            double sec = reactTimeMs /1000.0 / totalClicks;
+            MessageBox.Show("Среднее время реакции:  " + sec.ToString("0.000") + "сек.","Результат");
+            btnStart.Enabled = true;
+            clickNr = -1;
+
+        }
+    }
+}
