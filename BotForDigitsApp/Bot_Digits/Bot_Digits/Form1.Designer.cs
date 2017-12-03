7ca2049f07f5625610b1ded2dc6cb063ec60e719
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2017-12-03 02:08:52 +0200

    Mouse Clicker (Bot for Digits app)

---
+++ BotForDigitsApp/Bot_Digits/Bot_Digits/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Bot_Digits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRepeat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.Color.Moccasin;
+            this.textBoxX.Location = new System.Drawing.Point(12, 12);
+            this.textBoxX.Multiline = true;
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.ReadOnly = true;
+            this.textBoxX.Size = new System.Drawing.Size(100, 285);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.BackColor = System.Drawing.Color.Moccasin;
+            this.textBoxY.Location = new System.Drawing.Point(118, 12);
+            this.textBoxY.Multiline = true;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.ReadOnly = true;
+            this.textBoxY.Size = new System.Drawing.Size(100, 285);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(234, 12);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(160, 40);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(234, 108);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(160, 83);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить координаты мышки";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRepeat
+            // 
+            this.buttonRepeat.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRepeat.Location = new System.Drawing.Point(234, 257);
+            this.buttonRepeat.Name = "buttonRepeat";
+            this.buttonRepeat.Size = new System.Drawing.Size(160, 40);
+            this.buttonRepeat.TabIndex = 4;
+            this.buttonRepeat.Text = "Повторить 9 раз";
+            this.buttonRepeat.UseVisualStyleBackColor = true;
+            this.buttonRepeat.Click += new System.EventHandler(this.buttonRepeat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(406, 330);
+            this.Controls.Add(this.buttonRepeat);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Автокликер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRepeat;
+    }
+}
+
---
+++ BotForDigitsApp/Bot_Digits/Bot_Digits/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Bot_Digits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxX.ResetText();
+            textBoxY.ResetText();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = Cursor.Position.X;
+            y = Cursor.Position.Y;
+            textBoxX.Text += x.ToString() + Environment.NewLine;
+            textBoxY.Text += y.ToString() + Environment.NewLine;
+        }
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        
+
+        public void DoMouseClick(int x,int y)
+        {
+            Cursor.Position = new Point(x, y); 
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)x, (uint)y, 0, 0);
+        }
+
+        
+    
+    private void buttonRepeat_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < textBoxX.Lines.Length; j++)
+            {
+                for (int i = 0; i < textBoxX.Lines.Length-1; i++)
+                {
+                    int x, y;
+                    x = Convert.ToUInt16(textBoxX.Lines[i]);
+                    y = Convert.ToUInt16(textBoxY.Lines[i]);
+                    DoMouseClick(x, y);
+                }
+            }
+        }
+    }
+}
