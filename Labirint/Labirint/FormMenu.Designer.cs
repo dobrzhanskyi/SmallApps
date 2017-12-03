cb62038884a842b106ab01d46794f665aeaf46c7
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2017-12-03 22:28:47 +0200

    Labirint 2d
    Only menu,with 2 buttons and 1 checkbox(sound enable/disable)

---
+++ Labirint/Labirint/FormMenu.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Labirint
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.cbSound = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_start.Location = new System.Drawing.Point(12, 293);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(129, 61);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Начать игру";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_exit.Location = new System.Drawing.Point(324, 293);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(129, 61);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "Выход";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // cbSound
+            // 
+            this.cbSound.AutoSize = true;
+            this.cbSound.BackColor = System.Drawing.Color.Transparent;
+            this.cbSound.Checked = true;
+            this.cbSound.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbSound.ForeColor = System.Drawing.Color.Honeydew;
+            this.cbSound.Location = new System.Drawing.Point(192, 318);
+            this.cbSound.Name = "cbSound";
+            this.cbSound.Size = new System.Drawing.Size(77, 17);
+            this.cbSound.TabIndex = 2;
+            this.cbSound.Text = "Звук Есть";
+            this.cbSound.UseVisualStyleBackColor = false;
+            this.cbSound.CheckedChanged += new System.EventHandler(this.cbSound_CheckedChanged);
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Labirint.Properties.Resources.logo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(475, 376);
+            this.Controls.Add(this.cbSound);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMenu";
+            this.Text = "Лабиринт 2D";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.CheckBox cbSound;
+    }
+}
+
---
+++ Labirint/Labirint/FormMenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirint
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbSound_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbSound.Checked)
+            {
+                cbSound.Text = "Звук Есть";
+                Sound.SoundOn();
+            }
+            else
+            {
+                cbSound.Text = "Звука Нет";
+                Sound.SoundOff();
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            Sound.PlayStart();
+        }
+    }
+}
